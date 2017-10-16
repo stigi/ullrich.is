8afcdbe3e9cc6235df4b8a31e9302005b5f9bef5
Ullrich Schäfer <ullrich@seidbereit.de>
2017-10-16 14:14:11 +0200

    Adding ESLint

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ import { Content } from 'nextein/post'
 export default withPosts(({ posts }) => {
   return (
     <main>
-    {
-      posts.map((post, index) => (
-        <article key={`post-${index}`}>
-          <h1>{post.data.title}</h1>
-          <Content {...post} />
-        </article>
-      ))
+      {
+        posts.map((post, index) => (
+          <article key={`post-${index}`}>
+            <h1>{post.data.title}</h1>
+            <Content {...post} />
+          </article>
+        ))
     }
     </main>
   )
