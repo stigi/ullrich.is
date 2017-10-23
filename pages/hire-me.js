42c12d89c8e38eb93bdb337e8c33bdff56a6686e
Ullrich Schäfer <ullrich@seidbereit.de>
2017-10-23 15:52:44 +0200

    Extracting hire-me to Markdown

--- pages/hire-me.js
+++ pages/hire-me.js
@@ -1,42 +1,4 @@
 // @flow
-import React, {Component} from 'react'
-import Link from 'nextein/link'
-import withLayout from '../components/Layout'
+import contentFor from '../components/ContentPage'
 
-export default withLayout(({}) => (
-    <main>
-      <h1>
-        Work with me
-      </h1>
-      <p>
-        Here are some of the things I can help you and your team with:
-      </p>
-      <ul>
-        <li>
-          <h2>Good old software engineering</h2>
-          <p>
-            Building a v1.0, getting an MVP out the door, adding new features to an existing app. I am a good colaborator when it comes to working with PMs and designers, as well as other engineers.<br />
-            I'm focused on iOS software development in Swift and Objective-C. Lately I've gained more experience in Javascript and ReactNative, so don't hesitate pinging me about them as well.
-          </p>
-        </li>
-        <li>
-          <h2>Code and architecture review</h2>
-          <p>
-            Do you have an existing app an a team of developers working on it. I can help identify archetectural bottle necks that prevent your team from moving fast.<br/>
-            I can also review your teams newly written code and give instant feedback. There's no better way for an engineer to grow than by providing them instant feedback on their work.
-          </p>
-        </li>
-        <li>
-          <h2>Workshops and Talks</h2>
-          <p>
-            Tap my <Link as="/experienced" href="/portfolio">years of experience</Link> in the field, working sucessfully in different settings from small startups to the scale of Facebook. Learn from the wins and failures I've seen me and others do over time.
-            Let me suggest workflows and improvements to help your project excel.
-          </p>
-        </li>
-      </ul>
-      <p>
-        Interested in talking? I'm always <Link href="mailto:hi@ullrich.is?subject=Let's chat&body=Hi Ullrich 👋%0D%0A%0D%0A[√] Let me buy you a coffe/beer.%0D%0A[ ] Do you have time for lunch?%0D%0A[ ] I want you to meet the team.%0D%0A[ ] Urgent! Call me right now!">up for a meeting</Link> or to jump on the <Link href="tel:+491706164411">phone</Link>.
-      </p>
-    </main>
-  )
-)
+export default contentFor('hire-me')
---
+++ posts/hire-me.md
@@ -0,0 +1,24 @@
+---
+title: Hire Me
+category: hire-me
+---
+
+# Let's work together 💪
+
+Here are some of the things I can help you and your team with:
+
+*   ## Good old software engineering
+
+    Building a v1.0, getting an MVP out the door, adding new features to an existing app. I am a good colaborator when it comes to working with PMs and designers, as well as other engineers.  
+    I'm focused on iOS software development in Swift and Objective-C. Lately I've gained more experience in Javascript and ReactNative, so don't hesitate pinging me about them as well.
+
+*   ## Code and architecture review
+
+    Do you have an existing app an a team of developers working on it. I can help identify archetectural bottle necks that prevent your team from moving fast.  
+    I can also review your teams newly written code and give instant feedback. There's no better way for an engineer to grow than by providing them instant feedback on their work.
+
+*   ## Workshops and Talks
+
+    Tap my [years of experience](/experienced) in the field, working sucessfully in different settings from small startups to the scale of Facebook. Learn from the wins and failures I've seen me and others do over time. Let me suggest workflows and improvements to help your project excel.
+
+Interested in talking? I'm always [up for a meeting 📆](mailto:hi@ullrich.is?subject=Let's%20chat&amp;body=Hi%20Ullrich%20👋%0D%0A%0D) or to jump on the [phone 📱](tel:+491706164411).
