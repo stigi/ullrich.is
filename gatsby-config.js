aca8a63f6ea0d45e002cc6ccd8f34a35fa9ac81c
Ullrich Schäfer <git@ullrich.is>
2018-10-22 20:50:50 +0200

    Adding remark and filesyste plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,16 @@
 module.exports = {
   siteMetadata: {
-    name: `ullrich.is ✌️`,
-  },  
+    name: `ullrich.is ✌️`
+  },
   plugins: [
     `gatsby-plugin-typescript`,
-  ],
-}
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`
+      }
+    }
+  ]
+};
